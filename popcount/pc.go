1c1c5af45dab985b7149762fa582e62e7117c10e
vladkuz <vlad.meison@gmail.com>
2023-10-24 20:58:18 +0300

    'third'

---
+++ popcount/pc.go
@@ -0,0 +1,36 @@
+package popcount
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount_23(x uint64) int {
+	ans := 0
+	for i := 0; i < 8; i++ {
+		ans += int(pc[byte(x>>(i*8))])
+	}
+	return ans
+}
+
+func PopCount_24(x uint64) int {
+	n := 0
+	for i := uint(0); i < 64; i++ {
+		if x&(1<<i) != 0 {
+			n++
+		}
+	}
+	return n
+}
+
+func PopCount_25(x uint64) int {
+	n := 0
+	for x != 0 {
+		x = x & (x - 1) // clear rightmost non-zero bit
+		n++
+	}
+	return n
+}
